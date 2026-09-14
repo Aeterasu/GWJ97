63100f72662bb4d5ed9303b4f84b95d4d18a2e3f
Matthew <matveykerov@gmail.com>
2026-09-15 01:33:24 +0300

    high roller pattern

--- project/scoring/score_item.gd
+++ project/scoring/score_item.gd
@@ -40,7 +40,7 @@ const GRAVITY: float = 9.8 / 5.0
 const OOB_THRESHOLD: float = 32.0
 
 const ITEM_COLLECT_RADIUS: float = 96.0 * 96.0
-const SUN_COLLECT_RADIUS: float = 54.0 * 54.0
+const SUN_COLLECT_RADIUS: float = 36.0 * 36.0
 
 const COLLECT_DURATION: float = 0.8
 
