6e996fa2280afbd714411cedfc33b82064dd4a68
tercs <113709421+Terrces@users.noreply.github.com>
2026-09-14 17:47:01 +0500

    title changes

--- project/audio/bgm_manager.gd
+++ project/audio/bgm_manager.gd
@@ -2,8 +2,8 @@ class_name BGMManager extends Node
 
 enum BGMType
 {
-    MENU,
-    GAME,
+	MENU,
+	GAME,
 }
 
 @export var bgm_dict : Dictionary[BGMType, BGM] = {}
@@ -11,20 +11,20 @@ enum BGMType
 var current_bgm : AudioStreamPlayer = null
 
 func update_bgm(bgm : BGMType, reset : bool = false) -> void:
-    for type in bgm_dict.keys():
-        var player = bgm_dict[type]
+	for type in bgm_dict.keys():
+		var player = bgm_dict[type]
 
-        if type == bgm:
-            player.enable(reset)
-            current_bgm = player
-        else:
-            player.disable()
-            current_bgm = null
+		if type == bgm:
+			player.enable(reset)
+			current_bgm = player
+		else:
+			player.disable()
+			current_bgm = null
 
 func pause() -> void:
-    if current_bgm:
-        current_bgm.stream_paused = true
+	if current_bgm:
+		current_bgm.stream_paused = true
 
 func resume() -> void:
-    if current_bgm:
-        current_bgm.stream_paused = false
+	if current_bgm:
+		current_bgm.stream_paused = false
--- project/settings/settings_menu.gd
+++ project/settings/settings_menu.gd
@@ -10,26 +10,26 @@ class_name SettingsMenu extends Control
 var settings : SettingsManager = null
 
 func _ready() -> void:
-    settings = Main.instance.settings
+	settings = Main.instance.settings
 
-    self.grab_focus.call_deferred()
+	self.grab_focus.call_deferred()
 
-    back_to_menu.pressed.connect(on_back_to_menu)
+	back_to_menu.pressed.connect(on_back_to_menu)
 
-    if Main.is_in_web():
-        screen_shake.focus_neighbor_top = slider_3.get_path()
-        slider_3.focus_neighbor_bottom = screen_shake.get_path()
-        fullscreen.queue_free()
+	if Main.is_in_web():
+		screen_shake.focus_neighbor_top = slider_3.get_path()
+		slider_3.focus_neighbor_bottom = screen_shake.get_path()
+		fullscreen.queue_free()
 
 func _physics_process(delta: float) -> void:
-    if not get_viewport().gui_get_focus_owner():
-        self.grab_focus.call_deferred()
+	if not get_viewport().gui_get_focus_owner():
+		self.grab_focus.call_deferred()
 
-    if Input.is_action_just_pressed("ui_cancel"):
-        on_back_to_menu()
+	if Input.is_action_just_pressed("ui_cancel"):
+		on_back_to_menu()
 
 func on_back_to_menu() -> void:
-    settings.refresh_volume_config()
-    settings.save()
+	settings.refresh_volume_config()
+	settings.save()
 
-    await Main.instance.load_state(Main.State.TITLE)
+	await Main.instance.load_state(Main.State.TITLE)
