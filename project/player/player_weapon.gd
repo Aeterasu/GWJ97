a83bc91dfed5e908550d8e2c5a44927832a9387c
Matthew <matveykerov@gmail.com>
2026-09-15 02:53:39 +0300

    fix

--- project/player/player_weapon.gd
+++ project/player/player_weapon.gd
@@ -29,7 +29,7 @@ func _physics_process(delta: float) -> void:
 func fire() -> void:
 	for origin in shot_origins:
 		var offset: Vector2 = Vector2(
-			randf_range(-origin_random_offset.x, origin_random_offset.y),
+			randf_range(-origin_random_offset.x, origin_random_offset.x),
 			randf_range(-origin_random_offset.y, origin_random_offset.y))
 		var bullet = bullet_engine.fire_bullet(origin.global_position + offset, Vector2.UP.angle(), shot_speed, BulletSkin.Type.PLAYER_BULLET_DEFAULT)
 		bullet.damage = damage
