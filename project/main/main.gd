b9871db3e0b55b740e6d4ea61da8ab42e30eac22
tercs <113709421+Terrces@users.noreply.github.com>
2026-09-17 00:00:42 +0500

    debug check

--- project/main/main.gd
+++ project/main/main.gd
@@ -51,6 +51,11 @@ func _ready() -> void:
 	get_viewport().debug_draw = Viewport.DEBUG_DRAW_UNSHADED
 	get_viewport().audio_listener_enable_3d = false
 
+	if OS.is_debug_build():
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+	else:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+
 	settings = SettingsManager.new()
 	settings.load()
 
